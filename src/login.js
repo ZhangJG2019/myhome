c5a84479c7c609143f408c0bf9ef9e8539079333
zhangjuguang <1161401821@qq.com>
2019-03-09 21:22:54 +0800

    完成登录页面布局

--- src/login.js
+++ src/login.js
@@ -1,10 +1,50 @@
 //登录
 import React from 'react'
+import {
+	Form,
+	Icon,
+	Button,Divider
+} from 'semantic-ui-react'
+import './login.css'
 
-class Login extends React.Component{
-	render(){
+
+class Login extends React.Component {
+	render() {
 		return (
-			<div>登录</div>
+			<div className="login-container">
+				<div className="login-logo">
+					<Icon name='home' size='huge' />
+				</div>
+				<div className="login-form">
+					<Form>
+						<Form.Input 
+							fluid
+							icon='user' 
+							required 
+							size='big' 
+							iconPosition='left' 
+							name='username'
+							placeholder='请输入用户名...' 
+						/>
+						<Form.Input 
+							fluid
+							icon='user' 
+							required 
+							size='big' 
+							iconPosition='left' 
+							name='username'
+							placeholder='请输入密码...' 
+						/>
+						<Button className="login-button" fluid color="green">登录</Button>
+						<Button className="login-button" fluid color="green">取消</Button>
+						<Divider horizontal>---</Divider>
+					</Form>
+				</div>
+				<div className="login-third">
+					<Icon name='qq' size='big' />
+					<Icon name='chat' size='big' />
+				</div>
+			</div>
 		)
 	}
 }
